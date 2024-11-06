3f140a394dddbbf5bb988c6ac368521306fed9bf
Gal <galcesana8@gmail.com>
2024-11-06 18:53:00 +0200

    include template folder
    now you can choose different templates

---
+++ trys.py
@@ -0,0 +1,5 @@
+from cv_tailor import *
+
+resume_template = read_resume_template("C:/Users/galce/OneDrive/שולחן העבודה/FOLDERS/projects/cv matcher/SHEEP-TAILOR/resume templates/1.txt")
+
+print(resume_template)
